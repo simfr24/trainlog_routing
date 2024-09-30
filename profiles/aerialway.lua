d8b4969b80bcad906a632672fd75c863feffc38c
simfr24 <29752452+simfr24@users.noreply.github.com>
2024-09-30 12:40:43 +0200

    Fix aerialway speed

--- profiles/aerialway.lua
+++ profiles/aerialway.lua
@@ -2,21 +2,18 @@ api_version = 4
 
 function setup()
   return {
-    default_speed             = 120
-}
-
+    default_speed = 10
+  }
 end
 
-
 function process_way(profile, way, result, relations)
-    result.forward_speed = 1
-    result.backward_speed = 1
-    result.forward_rate = 1
-    result.backward_rate = 1
+    result.forward_speed = profile.default_speed
+    result.backward_speed = profile.default_speed
+    result.forward_rate = profile.default_speed
+    result.backward_rate = profile.default_speed
     result.forward_mode = mode.route
     result.backward_mode = mode.route
 end
-
 
 return {
     setup = setup,
